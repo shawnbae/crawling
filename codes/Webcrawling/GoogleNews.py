f1ca52d841bb016341289b37f94563dadf873e98
shawnbae <tngks0315@naver.com>
2021-03-25 18:45:40 +0900

    20210325 update

--- codes/Webcrawling/GoogleNews.py
+++ codes/Webcrawling/GoogleNews.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 base_url = "https://news.google.com"
 search_keyword = "파이썬"
-search_url = base_url + '/search?q=' + urllib.parse.quote(search_keyword)
+search_url = base_url + '/search?hl=ko&gl=KR&q=' + urllib.parse.quote(search_keyword)
 
 resp = requests.get(search_url)
 html_src = resp.text
@@ -14,14 +14,25 @@ news_items = soup.select('div[class="xrnccd"]')
 print(news_items[:3])
 
 for item in news_items[:3]:
-  link = item.find('a', attrs = {'class':'VDXfz'})
+  link = item.find('a', attrs = {'class':'VDXfz'}).get('href')
   news_link = base_url + link[1:]
   print(news_link)
+  print('\n')
   
   news_title = item.find('a', attrs = {'class':'DY5T1d'}).getText()
   print(news_title)
-  
+  print('\n')
+
   news_content = item.find('span', attrs = {'class':'xBbh9'}).text
   print(news_content)
+  print('\n')  
   
+  news_agency = item.find('a', attrs = {'class':'wEwyrc AVN2gc uQIVzc Sksgp'}).text
+  print(news_agency)
+  print('\n')
   
+  news_reporting = item.find('time', attrs = {'class':'WW6dff uQIVzc Sksgp'})
+  news_reporting_datetime = news_reporting.get('datetime').split('T')
+  news_reporting_date = news_reporting_datetime[0]
+  news_reporting_time = news_reporting_datetime[1][:-1]
+  print(news_reporting_date, news_reporting_time)
