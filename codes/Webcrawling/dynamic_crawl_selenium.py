4bde278d02ec637653f9c7839852c00f313d685d
shawnbae <tngks0315@naver.com>
2021-03-26 15:39:07 +0900

    20210326 update

--- codes/Webcrawling/dynamic_crawl_selenium.py
+++ codes/Webcrawling/dynamic_crawl_selenium.py
@@ -2,5 +2,21 @@ from selenium import webdriver
 
 driver = webdriver.Chrome("c:/chromedriver.exe")
 driver.implicitly_wait(3) # 3초 기다리기 
+driver.get("https://www.danawa.com/")
+
+# 다나와 로그인 버튼 누르기
+login =  driver.find_element_by_css_selector('li.my_page_service > a')
+print("HTML 요소: ", login)
+print("태그 이름: ", login.tag_name)
+print("문자열: ", login.text)
+print("href 속성: ", login.get_attribute('href'))
+
+login.click()
+driver.implicitly_wait(3)
+
+# 로그인하기 
+my_id = "내 id"
+my_pw = "내 password"
 
 
+driver.quit()
