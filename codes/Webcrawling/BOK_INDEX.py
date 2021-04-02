0e6a4b687dea3932e655aca557bc71ec8129ca1f
shawnbae <tngks0315@naver.com>
2021-04-02 20:36:56 +0900

    20210402 update

--- codes/Webcrawling/BOK_INDEX.py
+++ codes/Webcrawling/BOK_INDEX.py
@@ -34,8 +34,40 @@ def download_bok_statistics_by_keyword():
       keyword = str(input("검색할 항목을 입력하세요: "))
       
     # 웹드라이버 실행
+    driver = webdriver.Chrome("c:/chromedriver.exe")
+    driver.implicitly_wait(3)
+    driver.get("http://ecos.bok.or.kr/jsp/vis/keystat/#/key")
+    time.sleep(5)
     
+    items1 = driver.find_elements_by_css_selector('a[class="ng-binding"]')
+    items2 = driver.find_elements_by_css_selector('a[class="a-c1-list ng-binding"]')  
+    items3 = driver.find_elements_by_css_selector('a[class="a-c4-list ng-binding"]')  
+    driver.implicitly_wait(3)
+    
+    items = items[1:] + items2 + items3
+    
+    for idx, item in enumerate(items):
+      if keyword in item.text:
+        print("검색어 '%s'에 매칭되는 '%s' 통계지표 검색중" % (keyword, item.text))
+        item.click()
+        item_found = 1
+        time.sleep(5)
+        break
+      elif idx == (len(items) - 1):
+        print("검색어 '%s'에 대한 통계지표가 존재하지 않습니다" % keyword)
+        driver.close()
+        continue
+      else:
+        pass
       
-      
-      
-      
+    # 검색결과 로딩 HTML 웹 페이지를 파싱 - 통계지표 테이블(표) 양식 정리
+    html_src = driver.page_source
+    soup = BeautifulSoup(html_src, 'html.parser')
+    driver.close()
+    
+    table_items = soup.find_all('td', {'class':'ng-binding'})
+    date = [t.text for i, t in enumerate(table_items) if i % 3 == 0]
+    value = [t.text for i, t in enumerate(table_items) if i % 3 == 1]
+    change = [t.text for i, t in enumerate(table_items) if i % 3 == 2]
+    
+    # CSV형태의 파일로 저장하
