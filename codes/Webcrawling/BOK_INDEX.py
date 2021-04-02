aaad509818ba98e3eb063e32a88b8de259550ac3
shawnbae <tngks0315@naver.com>
2021-04-02 19:54:59 +0900

    20210402 update

--- codes/Webcrawling/BOK_INDEX.py
+++ codes/Webcrawling/BOK_INDEX.py
@@ -6,4 +6,14 @@ def download_bok_statistics():
   
   driver = webdriver.Chrome("c:/chromedriver.exe")
   driver.implicitly_wait(3)
+  driver.get("http://ecos.bok.or.kr/jsp/vis/keystat/#/key")
+  
+  excel_download = driver.find_element_by_css_selector("img[alt="download"]")
+  driver.implicitly_wait(3)
+  
+  excel_download.click()
+  time.sleep(5)
+  
+  driver.close()
+  print("파일 다운로드 실행 중")
   
