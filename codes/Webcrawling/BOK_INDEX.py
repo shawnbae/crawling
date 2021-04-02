52bed2639ac350999f59546fed5b0a9bc4aedf66
shawnbae <tngks0315@naver.com>
2021-04-02 19:52:44 +0900

    20210402 update

--- codes/Webcrawling/BOK_INDEX.py
+++ codes/Webcrawling/BOK_INDEX.py
@@ -2,4 +2,8 @@ from selenium import webdriver
 import time
 
 # 100대 통계지표 엑셀로 다운로드하기
-def 
+def download_bok_statistics():
+  
+  driver = webdriver.Chrome("c:/chromedriver.exe")
+  driver.implicitly_wait(3)
+  
