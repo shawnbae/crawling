107e508989fc9a8c3140210128f7d30436227c9b
shawnbae <tngks0315@naver.com>
2021-04-02 20:06:15 +0900

    20210402 update

--- codes/Webcrawling/BOK_INDEX.py
+++ codes/Webcrawling/BOK_INDEX.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import time
 
 # 100대 통계지표 엑셀로 다운로드하기
@@ -8,7 +9,7 @@ def download_bok_statistics():
   driver.implicitly_wait(3)
   driver.get("http://ecos.bok.or.kr/jsp/vis/keystat/#/key")
   
-  excel_download = driver.find_element_by_css_selector("img[alt="download"]")
+  excel_download = driver.find_element_by_css_selector('img[alt="download"]')
   driver.implicitly_wait(3)
   
   excel_download.click()
@@ -17,3 +18,7 @@ def download_bok_statistics():
   driver.close()
   print("파일 다운로드 실행 중")
   
+  return None
+
+download_bok_statistics()
+
