095b90e4660b38a60c22e0c54471bbf97db7a044
shawnbae <tngks0315@naver.com>
2021-04-21 20:35:19 +0900

    20210421 update

--- codes/Webcrawling/googlelogin.py
+++ codes/Webcrawling/googlelogin.py
@@ -3,25 +3,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
 import time
+import pyperclip
 
 your_google_id = ''
 your_google_password = ''
 
 driver = webdriver.Chrome("c:/chromedriver.exe")
-url = "https://google.com"
+url = "https://accounts.google.com/signin/v2/identifier?hl=ko&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
 driver.get(url)
 driver.maximize_window()
 action = ActionChains(driver)
 
-driver.find_element_by_css_selector('.link_login').click()
-action.send_keys(your_naver_id).key_down(Keys.TAB).send_keys(your_naver_password).perform()
-action.reset_actions()
+driver.find_element_by_css_selector('.whsOnd.zHQkBf').send_keys(your_google_password)
+driver.find_element_by_css_selector('.VfPpkd-RLmnJb').click()
 
-driver.find_element_by_css_selector('.btn_global').click()
+(
+action.send_keys('@gmail.com').pause(2).key_down(Keys.TAB).key_down(Keys.TAB)
+.send_keys('제목입니다.').key_down(Keys.TAB)
+.send_keys('abcde').key_down(Keys.ENTER)
+.key_down(Keys.SHIFT).send_keys('abcde')
+.perform()
+)
 
-
-
-# driver.find_element_by_class_name('gb_g').click()
-
-# time.sleep(3)
-# driver.quit()
+time.sleep(3)
+driver.quit()
